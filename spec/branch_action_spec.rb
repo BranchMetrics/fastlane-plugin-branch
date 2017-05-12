1f6e6bab01660965fda9cc2127f4b32f102f9ef7
Jimmy Dee <jgvdthree@gmail.com>
2017-05-12 15:02:43 -0700

    Marked example test pending. Added coverage to .gitignore

--- spec/branch_action_spec.rb
+++ spec/branch_action_spec.rb
@@ -1,6 +1,7 @@
 describe Fastlane::Actions::SetupBranchAction do
   describe '#run' do
     it 'prints a message' do
+      pending "No meaningful tests yet"
       expect(Fastlane::UI).to receive(:message).at_least(1)
 
       Fastlane::Actions::SetupBranchAction.run({})
