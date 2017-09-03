c0ed0357f04f541da3364ebe1e9205e65daeee36
Jimmy Dee <jgvdthree@gmail.com>
2017-09-02 17:48:56 -0700

    [Fastlane] Branch SDK integration

--- examples/ios/BranchPluginExample/BranchPluginExample/AppDelegate.swift
+++ examples/ios/BranchPluginExample/BranchPluginExample/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Branch Metrics. All rights reserved.
 //
 
+import Branch
 import UIKit
 
 @UIApplicationMain
@@ -14,8 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Branch.getInstance().initSession(launchOptions: launchOptions) {
+            universalObject, linkProperties, error in
+
+            // TODO: Route Branch links
+        }
 
         return true
     }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+      return Branch.getInstance().continue(userActivity)
+    }
+
 }
 
