709a2191856be6022ebfb190fba5749708e4f15e
Jimmy Dee <jgvdthree@gmail.com>
2017-11-20 12:58:09 -0800

    Move the generated Fastlane action pages from docs to doc (in .gitignore)

--- actions/update_action_docs.rb
+++ actions/update_action_docs.rb
@@ -4,7 +4,7 @@ module Fastlane
       class << self
         def run(params)
           template = File.read(File.join(Fastlane::ROOT, "lib", "assets", "ActionDetails.md.erb"))
-          FileUtils.mkdir_p "docs"
+          FileUtils.mkdir_p "doc"
 
           Dir[File.expand_path(File.join("..", "..", "lib", "fastlane", "plugin", "branch", "actions", "*_action.rb"), __FILE__)]
             .each do |path|
@@ -12,7 +12,7 @@ module Fastlane
             @action = Object.const_get("Fastlane").const_get("Actions").const_get(class_name)
             document = ERB.new(template, 0, '-').result binding
 
-            doc_path = File.join("docs", "#{File.basename(path, '.rb')}.md")
+            doc_path = File.join("doc", "#{File.basename(path, '.rb')}.md")
             File.write doc_path, document
           end
         end
